914735ca6d902209ea1d9721f8b0bb942412e391
ILoveEgg0001 <98178807+ILoveEgg0001@users.noreply.github.com>
2023-08-29 14:53:16 -0700

    Modified addBookToLibrary Function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const myLibrary = [];
 
+// Book Constructor
 function Book(title, author, pages, read) {
   this.title = title,
   this.author = author,
@@ -10,44 +11,57 @@ function Book(title, author, pages, read) {
   }
 }
 
-function addBookToLibrary(title, author, pages, read) {
+// addBookToLibrary Function
+function addBookToLibrary([title, author, pages, read]) {
   let book = new Book(title, author, pages, read)
   myLibrary.push(book);
 }
 
-addBookToLibrary("Book1", "J.R.R. Tolkien", 1295, "Read");
-addBookToLibrary("Book2", "DH", 2295, "Not Read");
-addBookToLibrary("Book3", "BB", 2395, "Read");
-addBookToLibrary("Book4", "AA", 2945, "Not Read");
-
-
-const titleColumn = document.querySelector(".title");
-const authorColumn = document.querySelector(".author");
-const pagesColumn = document.querySelector(".pages");
-const readColumn = document.querySelector(".read");
-
+// DisplayBook Function
 function displayBook() {
-  myLibrary.forEach((book) => {
-    const newTitleDiv = document.createElement("div");
-    const newTitleContent = document.createTextNode(book.title);
-    newTitleDiv.appendChild(newTitleContent);
-    titleColumn.appendChild(newTitleDiv);
+  const content = document.querySelector(".content");
+  content.textContent = "";
+  const propertiesToDisplay = ["title", "author", "pages", "read"];
 
-    const newAuthorDiv = document.createElement("div");
-    const newAuthorContent = document.createTextNode(book.author);
-    newAuthorDiv.appendChild(newAuthorContent);
-    authorColumn.appendChild(newAuthorDiv);
+  for (let i = 0; i < myLibrary.length; i++) {
+    const rowDiv = document.createElement("div");
+    rowDiv.id = i;
+    content.appendChild(rowDiv);
 
-    const newPagesDiv = document.createElement("div");
-    const newPagesContent = document.createTextNode(book.pages);
-    newPagesDiv.appendChild(newPagesContent);
-    pagesColumn.appendChild(newPagesDiv);
+    propertiesToDisplay.forEach((property) => {
+      const book = myLibrary[i];
+      const newBookDiv = document.createElement("div");
+      const newBookValue = document.createTextNode(book[property]);
+      rowDiv.appendChild(newBookDiv);
+      newBookDiv.appendChild(newBookValue);
+      });
+    };
+  };
 
-    const newReadDiv = document.createElement("div");
-    const newReadContent = document.createTextNode(book.read);
-    newReadDiv.appendChild(newReadContent);
-    readColumn.appendChild(newReadDiv);
-  });
-}
 
-displayBook();
+// Dialog
+const addaBookButton = document.getElementById("addaBook");
+const dialog = document.getElementById("bookDialog");
+const cancelButton = document.getElementById("cancel")
+const submitButton = document.getElementById("submit")
+const form = document.getElementById("form");
+
+addaBookButton.addEventListener("click", () => {
+  dialog.show();
+});
+
+cancelButton.addEventListener("click", () => {
+  dialog.close();
+});
+
+form.addEventListener("submit", (e) => {;
+  e.preventDefault();
+  const data = new FormData(form);
+  const newArray = [];
+  for (const value of data) {  
+    newArray.push(value.slice(1).toString());
+    }
+    addBookToLibrary(newArray);    
+    displayBook()
+  dialog.close();
+})
