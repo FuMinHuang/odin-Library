5e8e1fdabeb12d00519f7a075dab3a954a66415f
ILoveEgg0001 <98178807+ILoveEgg0001@users.noreply.github.com>
2023-09-13 08:49:14 -0700

    Refactor the book plain constructor to class

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const myLibrary = [];
 
-// Book Constructor
-function Book(title, author, pages, read) {
-  this.title = title,
-  this.author = author,
-  this.pages = pages,
-  this.read = read,
-  this.info = function() {
-    return (`${this.title} by ${this.author}, ${this.pages} pages, ${this.read}.`)
+// Class: Create Books
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title,
+    this.author = author,
+    this.pages = pages,
+    this.read = read,
+    this.info = function() {
+      return (`${this.title} by ${this.author}, ${this.pages} pages, ${this.read}.`)
+    }
   }
 }
 
